01eb785730b1998e4652e5d5ba183ce3e126b6e9
Maxym Horobets <mxm.horobets@gmail.com>
2022-11-04 16:29:27 +0200

    Закончил роутер

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { publicRoutes } from '../routes'
+import { SHOP_ROUTE } from '../utils/consts';
 
 const AppRouter = () => {
   return (
@@ -8,6 +9,7 @@ const AppRouter = () => {
         {publicRoutes.map(({path, Component}) => 
             <Route key={path} path={path} element={<Component/>} exact/>
         )}
+        <Route path='*' element={<Navigate to={SHOP_ROUTE} />}/>
     </Routes>
   )
 }
